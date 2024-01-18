b09514ec99bb41c4b9337bb64e37d2e61a383fcd
Shah M Polash <96693824+shahmpolash@users.noreply.github.com>
2024-01-18 12:43:23 +0600

    update port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4999;
 
 
 app.use(cors());
